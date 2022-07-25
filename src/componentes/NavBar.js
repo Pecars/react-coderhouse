d2a2c4dc86f8eb79671dd4657e5d498f33d73b3c
pecars <you@example.com>
2022-07-25 22:26:26 +0100

    sube menú e-commerce

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = (parametros) => {
+
+      return (
+        <>
+        <h1>título </h1>
+        <nav>
+          <a href="#">categoría 1</a>
+          <a href="#">categoría 2</a>
+          <a href="#">categoría 3</a>
+        </nav>
+        </>
+      )
+  }
+  
+  export default NavBar
